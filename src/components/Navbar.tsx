e35c1de8a3e8e6971bf64c3c8455cbaa3a663171
BÜŞRA ÇERÇER <busra_cercer@outlook.com>
2023-12-12 15:27:35 +0300

    navbar dosyası eklendi.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { IAdmin } from '../models/IAdmin'
+
+function Navbar(item : {admin :IAdmin}) {
+  const navigate = useNavigate()
+  useEffect(() => {
+    if(item.admin){
+
+    }else{
+      navigate('/')
+    }
+  
+    
+  }, [])
+  
+    
+  const logout = () => {
+
+    
+    sessionStorage.removeItem("admin")
+    navigate('/')
+
+
+  }
+
+  return (
+    <>
+     <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink className="nav-link" to='/home'>Home</NavLink>
+          
+        </li>
+        <li className="nav-item">
+        
+          <NavLink className="nav-link" to ={'/basket'}>Basket</NavLink>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item"onClick={logout} role='button'>LOGOUT</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">{item.admin && item.admin.firstName} { item.admin && item.admin.lastName}</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
