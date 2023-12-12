85e8e5cf8530c803cf259a4cd5efa6417fa566b5
BÜŞRA ÇERÇER <busra_cercer@outlook.com>
2023-12-12 17:04:46 +0300

    2 dosyada değiştirildi.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ function Navbar(item : {admin :IAdmin}) {
     <>
      <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">NAVBAR</a>
+    <a className="navbar-brand" href="#">NAVBAR GIT</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
